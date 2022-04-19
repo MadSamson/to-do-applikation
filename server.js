8f68666364418cbb8aa9ab0ce73d4cd85356185c
MadSamson <m.mahdi.samadi@gmail.com>
2022-04-19 09:15:44 +0200

    setting up the database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,7 @@ app.get('/', (req,res)=>{
     res.send('hello world')
 })
 
+mongoose.connect('mongodb://localhost/todo-assignment')
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 })
